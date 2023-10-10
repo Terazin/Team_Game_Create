249f34b32f675ca47bd2a9753f5ca33bcfef6bf7
Terazin <131113249+Terazin@users.noreply.github.com>
2023-10-10 18:04:14 +0900

    ゲーム画面とメインメニューの連携作業
    ゲームプレイ中にポーズ、メインメニュー画面に戻る等の移植を行った。

--- game/Assets/Script/GamePause.cs
+++ game/Assets/Script/GamePause.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GamePause : MonoBehaviour
 {
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Button resumeButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pausePanel.SetActive(false);
+        resumeButton.onClick.AddListener(Resume);
     }
 
     // Update is called once per frame
@@ -16,7 +20,15 @@ public class GamePause : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Escape))
         {
-            SceneManager.LoadScene("GameScene");
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
         }
     }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
 }
