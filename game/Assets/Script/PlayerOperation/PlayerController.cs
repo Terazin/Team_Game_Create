8dc3617d38659f29eef0bc62b187c4e8b6906f83
zakitowa1210 <g2227077@st.t-kougei.ac.jp>
2023-10-17 18:45:13 +0900

    コントローラーでの制御
    まだ完璧じゃないお

--- game/Assets/Script/PlayerOperation/PlayerController.cs
+++ game/Assets/Script/PlayerOperation/PlayerController.cs
@@ -1,30 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform camTransform;
     public float walkSpeed;
-    private float sspeed;
+    private float speed;
     private Vector3 movement;
     private CharacterController controller;
 
-    
-
-    // ★追加（方向転換）
+    // 方向転換用
     private Vector3 moveDir = Vector3.zero;
     private float gravity = 3f;
+
+    // カメラの回転用
+    public float turnSpeed = 150f; // 追加: この数値を変更して、カメラの回転速度を調整できます。
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-
-        sspeed = walkSpeed;
+        speed = walkSpeed;
     }
 
     void Update()
     {
         PlayerMove();
+        CameraTurn(); // 追加: カメラの回転処理を呼び出し
     }
 
     void PlayerMove()
@@ -33,15 +34,24 @@ public class PlayerController : MonoBehaviour
         float moveV = Input.GetAxis("Vertical");
         movement = new Vector3(moveH, 0, moveV);
 
-        // ★追加（方向転換）
-        Vector3 desiredMove = Camera.main.transform.forward * movement.z + Camera.main.transform.right * movement.x;
+        Vector3 desiredMove = camTransform.forward * movement.z + camTransform.right * movement.x;
         moveDir.x = desiredMove.x * 3f;
         moveDir.z = desiredMove.z * 3f;
-        moveDir.y -= gravity * Time.deltaTime; // 疑似重力の実装
+        moveDir.y -= gravity * Time.deltaTime;
 
-        // ★改良（方向転換）
-        controller.Move(moveDir * Time.deltaTime * sspeed);
+        controller.Move(moveDir * Time.deltaTime * speed);
+    }
+
+    // 追加: カメラの回転を制御
+    void CameraTurn()
+    {
+        float turnH = Input.GetAxis("RightStick Horizontal");
+        float turnV = Input.GetAxis("RightStick Vertical");
+
+        Debug.Log(turnH);
 
 
+        camTransform.Rotate(Vector3.up * turnH * turnSpeed * Time.deltaTime); // 水平方向の回転
+        camTransform.Rotate(Vector3.right * -turnV * turnSpeed * Time.deltaTime); // 垂直方向の回転（マイナスを使って上下の動きを反転）
     }
 }
