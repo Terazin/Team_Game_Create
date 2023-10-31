705c50f84c1c04b49bd8bbf9932e29e0f8cc2c93
zakitowa1210 <g2227077@st.t-kougei.ac.jp>
2023-10-31 16:54:20 +0900

    弾の発射
    RTボタンで弾の発射
    向いてる向きに弾を打つ

--- game/Assets/Script/PlayerOperation/ShotGun.cs
+++ game/Assets/Script/PlayerOperation/ShotGun.cs
@@ -8,6 +8,7 @@ public class ShotGun : MonoBehaviour
     public GameObject bulletPrefab;
     //public AudioClip shotSound;
     public float shotSpeed;
+<<<<<<< Updated upstream
     private bool shotBullet = false;
 
     void Update()
@@ -20,11 +21,38 @@ public class ShotGun : MonoBehaviour
                 GameObject bullet = Instantiate(bulletPrefab, transform.position, bulletGun.transform.rotation);
                 Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
                 bulletRb.AddForce(transform.forward * shotSpeed);
+=======
 
-                //AudioSource.PlayClipAtPoint(shotSound, Camera.main.transform.position);
+    private bool wasTriggerPressed = false; // 前回のフレームでRTボタンが押されていたかのフラグ
 
+    void Update()
+    {
+        // コントローラーのRTボタンでの入力を取得
+        bool isTriggerPressed = Input.GetAxis("TriggerRight") > 0.1f;
+>>>>>>> Stashed changes
+
+        // 前回のフレームでは押されておらず、現在のフレームで押されている場合に弾を発射
+        if (!wasTriggerPressed && isTriggerPressed)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, bulletGun.transform.rotation);
+            Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+            bulletRb.AddForce(transform.forward * shotSpeed);
+
+            //AudioSource.PlayClipAtPoint(shotSound, Camera.main.transform.position);
+        }
+
+        // フラグの更新
+        wasTriggerPressed = isTriggerPressed;
+
+        // キャラクターのy軸の回転をカメラのy軸の回転に一致させる
+        Vector3 newRotation = new Vector3(transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, transform.eulerAngles.z);
+        transform.eulerAngles = newRotation;
+
+<<<<<<< Updated upstream
                 shotBullet = true;
             }
         }
+=======
+>>>>>>> Stashed changes
     }
 }
