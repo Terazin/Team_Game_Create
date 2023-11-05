f6380362f37791e2ac0f7d64fa65dc869284f634
Terazin <131113249+Terazin@users.noreply.github.com>
2023-11-05 18:41:29 +0900

    ステージ6までの制作と弾のカメラ誘導
    ステージ6までの配置をしました。ワープギミックはまだ無いので置けていません。また、カメラを発射した際に視点をカメラに代わるようにしました。その際にShortgunスクリプトに少し変更を加えました。コードを消したりはせずあらたに加えた形でコメントアウトもあるのでおそらく追加部分はすぐわかると思います。

--- game/Assets/Script/PlayerOperation/ShotGun.cs
+++ game/Assets/Script/PlayerOperation/ShotGun.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,22 +10,63 @@ public class ShotGun : MonoBehaviour
     //public AudioClip shotSound;
     public float shotSpeed;
     //private bool shotBullet = false;
+    public CinemachineVirtualCamera virtualCamera; // Virtual Cameraをインスペクタから設定する
+    public Transform playerTransform; // PlayerのTransformをインスペクタから設定する
+    public float switchBackDelay = 2.0f; // カメラがプレイヤーに戻るまでの時間
+
+    private Transform originalFollowTarget;
+
+    void Start()
+    {
+        // カメラの元のFollowターゲットを保持
+        originalFollowTarget = virtualCamera.Follow;
+        if (virtualCamera.LookAt == null)
+        {
+            virtualCamera.LookAt = playerTransform; // プレイヤーのTransformをLookAtターゲットとして設定
+        }
+    }
 
     void Update()
     {
         //if (!shotBullet)  //弾を一発撃つと、shotBulletがtrueになって撃てなくなる。
         //{
-            // マウス左クリックで発射
-            if (Input.GetMouseButtonDown(0))
-            {
-                GameObject bullet = Instantiate(bulletPrefab, transform.position, bulletGun.transform.rotation);
-                Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
-                bulletRb.AddForce(transform.forward * shotSpeed);
+        // マウス左クリックで発射
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, bulletGun.transform.rotation);
+            Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+            bulletRb.AddForce(transform.forward * shotSpeed);
 
-                //AudioSource.PlayClipAtPoint(shotSound, Camera.main.transform.position);
+            //AudioSource.PlayClipAtPoint(shotSound, Camera.main.transform.position);
 
-                //shotBullet = true;
-            }
+            //shotBullet = true;
+
+            // カメラのFollowを弾に変更
+            virtualCamera.Follow = bullet.transform;
+
+            // 弾にカメラをフォーカスした後、一定時間でカメラをプレイヤーに戻す
+            StartCoroutine(SwitchBackToPlayerAfterDelay());
+        }
         //}
+    }
+
+    private IEnumerator SwitchBackToPlayerAfterDelay()
+    {
+        yield return new WaitForSeconds(switchBackDelay);
+
+        Debug.Log("Switching back to player.");
+
+    // デバッグログを出力して、カメラがプレイヤーに戻ったかを確認
+    if (virtualCamera.Follow == originalFollowTarget)
+    {
+        Debug.Log("Camera has switched back to the player.");
+    }
+    else
+    {
+        Debug.Log("Camera did not switch back to the player.");
+    }
+
+        // カメラのFollowをプレイヤーに戻す
+        virtualCamera.Follow = originalFollowTarget;
     }
 }
