293910c185cb7eb61b1d20a43b6dde5c50939cec
su11mooo <g2227040@st.t-kougei.ac.jp>
2023-12-05 16:31:30 +0900

    敵
    敵のサーチライト的な奴と三回までの猶予

--- game/Assets/Script/EnemySee/Observer.cs
+++ game/Assets/Script/EnemySee/Observer.cs
@@ -8,8 +8,11 @@ public class Observer : MonoBehaviour
     public Transform player;
     public GameObject GameOverWindow;
     private float lockRange = 200;
-    bool m_IsPlayerInRange;
-    //bool isGamePaused = false; // ゲームの一時停止状態
+    private bool m_IsPlayerInRange;
+    private bool isCooldown = false; // 検知のクールダウン状態
+    private int detectionCount = 0; // プレイヤーが検知された回数
+    private int maxDetectionCount = 3; // ゲームオーバーになる検知の最大回数
+    private float cooldownDuration =2.0f; // 次の検知までのクールダウン時間
 
     void OnTriggerEnter(Collider other)
     {
@@ -29,20 +32,37 @@ public class Observer : MonoBehaviour
 
     void Update()
     {
-        if (m_IsPlayerInRange)
+        if (m_IsPlayerInRange && !isCooldown)
         {
             Vector3 direction = player.position - transform.position + Vector3.up;
             Ray ray = new Ray(transform.position, direction);
             RaycastHit raycastHit;
 
-            if (Physics.Raycast(ray, out raycastHit, lockRange)) 
+            if (Physics.Raycast(ray, out raycastHit, lockRange))
             {
                 if (raycastHit.collider.transform == player)
                 {
-                    GameOverWindow.SetActive(true);
-                    SetGamePaused(true); // ゲームの時間を停止
-                    PlayerPrefs.SetInt("IsGamePaused", 1);
+                    detectionCount++; // プレイヤーが検知された回数を増やす
+                    Debug.Log("見つかったよ");
+                    if (detectionCount >= maxDetectionCount)
+                    {
+                        GameOverWindow.SetActive(true);
+                        SetGamePaused(true); // ゲームの時間を停止
+                        PlayerPrefs.SetInt("IsGamePaused", 1);
+                    }
+
+                    StartCooldown(); // クールダウンを開始
                 }
+            }
+        }
+
+        if (isCooldown)
+        {
+            cooldownDuration -= Time.deltaTime;
+            if (cooldownDuration <= 0)
+            {
+                isCooldown = false;
+                cooldownDuration = 5.0f; // クールダウン時間をリセット
             }
         }
     }
@@ -50,5 +70,10 @@ public class Observer : MonoBehaviour
     void SetGamePaused(bool isPaused)
     {
         Time.timeScale = isPaused ? 0.0f : 1.0f;
+    }
+
+    void StartCooldown()
+    {
+        isCooldown = true;
     }
 }
