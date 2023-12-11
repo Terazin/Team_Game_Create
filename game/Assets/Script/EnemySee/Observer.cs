6057ceaa582d14fa7bacce000a2716d86f7100d7
su11mooo <g2227040@st.t-kougei.ac.jp>
2023-12-11 15:47:32 +0900

    発見、ワープ
    発見関係諸々、ワープ

--- game/Assets/Script/EnemySee/Observer.cs
+++ game/Assets/Script/EnemySee/Observer.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Imageコンポーネントを使用するために追加
 using UnityEngine.SceneManagement;
 
 public class Observer : MonoBehaviour
 {
     public Transform player;
     public GameObject GameOverWindow;
+    public Image canvasImage; // キャンバス上のImageコンポーネント
+    public Image canvasImage2;
+    public Image canvasImage3;
     private float lockRange = 200;
     private bool m_IsPlayerInRange;
     private bool isCooldown = false; // 検知のクールダウン状態
     private int detectionCount = 0; // プレイヤーが検知された回数
     private int maxDetectionCount = 3; // ゲームオーバーになる検知の最大回数
-    private float cooldownDuration =2.0f; // 次の検知までのクールダウン時間
+    private float cooldownDuration = 2.0f; // 次の検知までのクールダウン時間
 
     void OnTriggerEnter(Collider other)
     {
@@ -43,7 +47,14 @@ public class Observer : MonoBehaviour
                 if (raycastHit.collider.transform == player)
                 {
                     detectionCount++; // プレイヤーが検知された回数を増やす
-                    Debug.Log("見つかったよ");
+                    Debug.Log("見つけたよ");
+
+                    // キャンバス上のImageコンポーネントの色を変更する
+                    if (canvasImage != null)
+                    {
+                        canvasImage.color = Color.red; // 例として赤色に変更
+                    }
+
                     if (detectionCount >= maxDetectionCount)
                     {
                         GameOverWindow.SetActive(true);
@@ -56,16 +67,31 @@ public class Observer : MonoBehaviour
             }
         }
 
+
+        if (detectionCount == 2)
+        {
+            canvasImage2.color = Color.red;
+        }
+
+        if (detectionCount == 3)
+        {
+            canvasImage3.color = Color.red;
+        }
+
         if (isCooldown)
         {
             cooldownDuration -= Time.deltaTime;
             if (cooldownDuration <= 0)
             {
                 isCooldown = false;
-                cooldownDuration = 5.0f; // クールダウン時間をリセット
+                cooldownDuration = 2.0f; // クールダウン時間をリセット
+
+                
             }
         }
     }
+
+
 
     void SetGamePaused(bool isPaused)
     {
